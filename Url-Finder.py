b44ad5ed7a2253b38f3db2f7b36bbfb86b0088e3
valerioviale <34212301+valerioviale@users.noreply.github.com>
2023-08-17 10:27:37 +0200

    Update Url-Finder.py
    Changed search engine to duckduckgo.
    Added time sleep to give to the search page time to load.
    Found the correct xpath to identify the first (relevant) search result).

--- Url-Finder.py
+++ Url-Finder.py
@@ -1,74 +1,65 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 import pandas as pd
-
+import os.path
 
 # List of companies
 companies = [
-    "	GESTORE DEI MERCATI ENERGETICI S.P.A.	"	,
-"	ENI S.P.A.	"	,
-"	STELLANTIS EUROPE S.P.A.	"	,
-"	ENEL GLOBAL TRADING S.P.A.	"	,
-"	ENI TRADING & SHIPPING S.P.A.	"	,
-"	GESTORE DEI SERVIZI ENERGETICI - GSE S.P.A.	"	,
-"	ENEL ENERGIA S.P.A.	"	,
-"	ENGIE ITALIA SPA	"	,
-"	KUWAIT PETROLEUM ITALIA S.P.A.	"	,
-"	TELECOM ITALIA SPA O TIM S.P.A.	"	,
-"	AMAZON WEB SERVICES EMEA SARL	"	,
-"	ALESSANDRO PATANE' SRL	"	,
-"	ESSO ITALIANA S.R.L.	"	
-
-    
+    "GESTORE DEI MERCATI ENERGETICI",
+    "ENI",
+    "STELLANTIS EUROPE",
+    "Ferrari automobili",
+    "Universita Milano Bicocca"
     # ... (add all other companies here)
 ]
 
+# File path for the Excel file
+excel_file_path = 'Url-Finder.xlsx'
 
 # Start the webdriver
-#driver = webdriver.Chrome("C:/Users/valerio/Desktop/Courses/LinkedinAuto/chromedriver.exe")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))  # starts the webdriver
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())  # starts the webdriver
 
 # Create a list to store company data
 company_data = []
 
+# Check if the Excel file exists
+if os.path.exists(excel_file_path):
+    # Load existing data from the Excel file
+    existing_df = pd.read_excel(excel_file_path)
+    company_data.extend(existing_df.to_dict(orient='records'))
+
 # Iterate through the list of companies
 for company in companies:
-    # Open Google search page
-    driver.get("https://www.google.com")
-    
+    # Open DuckDuckGo search page
+    driver.get("https://www.duckduckgo.com")
+
     # Find the search bar and type in the company name
     search_bar = driver.find_element(By.NAME, "q")
     search_bar.send_keys(company)
     search_bar.send_keys(Keys.RETURN)
-    
-    # Find the first search result (assuming it's the company's official website)
-    try:
-        result = driver.find_element(By.CSS_SELECTOR, "div.g a")
-        company_url = result.get_attribute("href")
-        company_data.append({"Company": company, "URL": company_url})
-    except:
-        company_data.append({"Company": company, "URL": "No URL found"})
 
-# Close the browser
-driver.quit()
+    # Wait for search results to load
+    time.sleep(3)
+    
+    # Find the span containing "https"
+    try:
+        url_span = driver.find_element(By.XPATH, "//span[contains(text(), 'https')]")
+        company_url = url_span.text
+    except:
+        company_url = "No URL found"
+    
+    company_data.append({"Company": company, "URL": company_url})
+    
+    time.sleep(2)  # Delay for 2 seconds before the next search
 
 # Create a DataFrame from the company data
 df = pd.DataFrame(company_data)
 
-# Read the existing file into a pandas dataframe
-try:
-        existing_df = pd.read_excel("URL-finder.xlsx")
-        # Concatenate the existing data with the new data
-        final_df = pd.concat([existing_df, df], ignore_index=True)
-except FileNotFoundError:
-        # If the file doesn't exist, just write the new data to it
-        final_df = df
+# Save the DataFrame to the Excel file
+df.to_excel(excel_file_path, index=False)
 
-    # Write the final data to the same excel file
-final_df.to_excel("URL-finder.xlsx", index=False, sheet_name='Sheet1', engine='openpyxl')
-
+# Close the browser
+driver.quit()
